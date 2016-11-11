cecae8e9c0c8f0b8186a3d19a701bb1ce75afc2e
Benjamin Watson <watsonb@gmail.com>
2016-11-11 01:15:38 -0500

    debug

--- rasterize.js
+++ rasterize.js
@@ -239,7 +239,7 @@ function setupWebGL() {
     // Set up keys
     document.onkeydown = handleKeyDown; // call this when key pressed
 
-    // Get the image canvas, render an image in it
+    /*// Get the image canvas, render an image in it
     var imageCanvas = document.getElementById("myImageCanvas"); // create a 2d canvas
     var cw = imageCanvas.width, ch = imageCanvas.height; 
     imageContext = imageCanvas.getContext("2d"); 
@@ -248,7 +248,7 @@ function setupWebGL() {
     bkgdImage.onload = function(){
         var iw = bkgdImage.width, ih = bkgdImage.height;
         imageContext.drawImage(bkgdImage,0,0,iw,ih,0,0,cw,ch);   
-    } // end onload callback 
+    } // end onload callback */
     
     // create a webgl canvas and set it up
     var webGLCanvas = document.getElementById("myWebGLCanvas"); // create a webgl canvas
